2b0ebc26bd210174d1207791a0226b4e0bdb8c0d
tobias.mcnulty <tobias.mcnulty@a8de2626-5439-11de-936b-e9512c4d2277>
2009-07-14 03:55:23 +0000

    initial import of django-crm (extracted from minibooks r430)

---
+++ trunk/crm/forms.py
@@ -0,0 +1,264 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# $Id: forms.py 425 2009-07-14 03:43:01Z tobias $
+# ----------------------------------------------------------------------------
+#
+#    Copyright (C) 2008-2009 Caktus Consulting Group, LLC
+#
+#    This file is part of django-crm and was originally extracted from minibooks.
+#
+#    django-crm is published under a BSD-style license.
+#    
+#    You should have received a copy of the BSD License along with django-crm.  
+#    If not, see <http://www.opensource.org/licenses/bsd-license.php>.
+#
+
+import datetime
+
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.localflavor.us import forms as us_forms
+from django.db import transaction
+from django.db.models import Q
+
+from caktus.django.forms import SimpleUserForm, RequestForm, RequestModelForm
+from caktus.django.widgets import CheckboxSelectMultipleWithJS
+from caktus.decorators import requires_kwarg
+from caktus.django import widgets as caktus_widgets
+
+import crm.models as crm
+
+class PersonForm(SimpleUserForm):
+    def clean_email(self):
+        if not self.instance.id and \
+          User.objects.filter(email=self.cleaned_data['email']).count() > 0:
+            raise forms.ValidationError('A user with that e-mail address already exists.')
+        return self.cleaned_data['email']
+
+
+class ProfileForm(forms.ModelForm):
+    """
+    Model form for user profiles.
+    """
+    
+    class Meta:
+        model = crm.Profile
+        fields = ('notes', 'picture')
+    
+    @transaction.commit_on_success
+    def save(self, user):
+        instance = super(ProfileForm, self).save(commit=False)
+        new_instance = not instance.id
+        instance.user = user
+        instance.save()
+        self.save_m2m()
+        return instance
+    
+    
+class PhoneForm(forms.ModelForm):
+    """
+    Model form for Phones.  The required type argument to __init__ should be
+    one of the types defined in Phone.PHONE_TYPES, and the label on the
+    form field will be set accordingly.  The profile is passed into save()
+    instead of __init__ because it may not exist at the time __init__
+    is called.
+    """
+    
+    class Meta:
+        model = crm.Phone
+        fields = ('number')
+    
+    @requires_kwarg('type')
+    def __init__(self, *args, **kwargs):
+        self.type = kwargs.pop('type')
+        super(PhoneForm, self).__init__(*args, **kwargs)
+        
+        self.fields['number'].label = \
+          dict(crm.Phone.PHONE_TYPES)[self.type]
+        if self.type != 'fax':
+            self.fields['number'].label += " phone"
+        self.fields['number'].required = False
+        
+    @transaction.commit_on_success
+    def save(self, profile):
+        instance = super(PhoneForm, self).save(commit=False)
+        new_instance = not instance.id
+        if self.cleaned_data['number']:
+            instance.profile = profile
+            instance.type = self.type
+            instance.save()
+            self.save_m2m()
+        else:
+            if not new_instance:
+                # if the number was removed, delete the instance
+                instance.delete()
+            instance = None
+        return instance
+
+
+class EmailForm(forms.Form):
+    to = forms.ChoiceField()
+    memo = forms.CharField(max_length=4096, widget=forms.Textarea)
+    
+    def clean_to(self):
+        try:
+            return User.objects.get(pk=self.cleaned_data['to'])
+        except User.DoesNotExist:
+            raise forms.ValidationError(_(u'This username is already taken. Please choose another.'))
+        
+        return self.cleaned_data['to']
+    
+    def __init__(self, *args, **kwargs):
+        search = None
+        
+        business = kwargs.pop('business', None)
+        project = kwargs.pop('project', None)
+        
+        if project:
+            # ignore business contacts if a project is set
+            search = Q(projects=project)
+        elif business:
+            search = Q(businesses=business)
+        
+        forms.Form.__init__(self, *args, **kwargs)
+        
+        if search:
+            self.fields['to'].choices = []
+            for user in User.objects.filter(search):
+                choice = (user.id, "%s (%s)" % (user.get_full_name(), user.email))
+                self.fields['to'].choices.append(choice)
+
+
+class UserModelChoiceField(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return obj.get_full_name()
+
+class InteractionForm(RequestModelForm):
+    class Meta:
+        model = crm.Interaction
+        fields = ('date', 'type', 'completed', 'project', 'contacts', 'memo',)
+    
+    def __init__(self, *args, **kwargs):    
+        self.url = kwargs.pop('url')
+        self.person = kwargs.pop('person')
+        self.crm_user = kwargs.pop('crm_user')
+        super(InteractionForm, self).__init__(*args, **kwargs)
+        self.fields.keyOrder = \
+            ('date', 'type', 'completed', 'contacts', 'project', 'memo',)
+        
+        self.fields['contacts'] = UserModelChoiceField(
+            widget=caktus_widgets.AjaxSelectMultiWidget(url=self.url),
+            queryset=User.objects.all(),
+        )
+        if not self.request.POST:
+            if self.instance.id:
+                initial_choices = \
+                    self.instance.contacts.values_list('id', flat=True)
+            else:
+                initial_choices = [self.person.user.id, self.crm_user.id]
+            self.fields['contacts'].widget.initial_choices = \
+                [unicode(choice) for choice in initial_choices]
+        
+        if not self.instance.id and self.person:
+            projects = crm.Project.objects.filter(contacts__profile=self.person)
+        elif self.instance.id:
+            # show only client projects
+            client_contacts = self.instance.contacts.filter(
+                businesses__business_types__name__iexact='client'
+            )
+            projects = crm.Project.objects.filter(
+                contacts__in=client_contacts
+            ).distinct()
+        else:
+            projects = crm.Project.objects.none()
+        
+        self.fields['project'].queryset = projects
+        
+        self.fields['date'].widget = caktus_widgets.MooDate()
+        self.fields['date'].initial = datetime.datetime.now()
+        
+    def save(self):
+        created = not self.instance.id
+        instance = super(InteractionForm, self).save()
+        if created:
+            if self.person:
+                instance.contacts.add(self.person.user)
+            if self.crm_user:
+                instance.contacts.add(self.crm_user)
+        return instance
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(required=False)
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = crm.Business
+        fields = ('name', 'notes', 'business_types')
+    
+    def __init__(self, *args, **kwargs):
+        super(BusinessForm, self).__init__(*args, **kwargs)
+        
+        self.fields['business_types'].label = 'Type(s)'
+        self.fields['business_types'].widget = \
+          caktus_widgets.CheckboxSelectMultipleWithJS(
+            choices = self.fields['business_types'].choices
+        )
+        self.fields['business_types'].help_text = '' 
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = crm.Project
+        fields = (
+            'name',
+            'business',
+            'trac_environment',
+            'point_person',
+            'type',
+            'status',
+            'description',
+        )
+
+    @requires_kwarg('business')
+    def __init__(self, *args, **kwargs):
+        self.business = kwargs.pop('business')
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        
+        if self.business:
+            self.fields.pop('business')
+        else:
+            self.fields['business'].queryset = crm.Business.clients.all()
+    
+    def save(self):
+        instance = super(ProjectForm, self).save(commit=False)
+        if self.business:
+            instance.business = self.business
+        instance.save()
+        return instance
+
+
+class AddressForm(forms.ModelForm):
+    """
+    Model form for US postal addresses.
+    """
+    class Meta:
+        model = crm.Address
+        fields = ('street', 'city', 'state', 'zip',)
+    
+    def __init__(self, *args, **kwargs):
+        super(AddressForm, self).__init__(*args, **kwargs)
+
+
+class ProjectRelationshipForm(RequestModelForm):
+    class Meta:
+        model = crm.ProjectRelationship
+        fields = ('types')
+    
+    def __init__(self, *args, **kwargs):
+        super(ProjectRelationshipForm, self).__init__(*args, **kwargs)
+        self.fields['types'].widget = forms.CheckboxSelectMultiple(
+            choices=self.fields['types'].choices
+        )
+        self.fields['types'].help_text = ''
---
+++ trunk/crm/models.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# $Id: models.py 425 2009-07-14 03:43:01Z tobias $
+# ----------------------------------------------------------------------------
+#
+#    Copyright (C) 2008-2009 Caktus Consulting Group, LLC
+#
+#    This file is part of django-crm and was originally extracted from minibooks.
+#
+#    django-crm is published under a BSD-style license.
+#    
+#    You should have received a copy of the BSD License along with django-crm.  
+#    If not, see <http://www.opensource.org/licenses/bsd-license.php>.
+#
+
+from django.db import models
+from django.contrib.auth.models import User, Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sites.models import Site
+from django.contrib.localflavor.us import models as us_models
+from django.conf import settings
+
+from caktus.django.db.util import slugify_uniquely
+
+
+class Address(models.Model):
+    street = models.TextField()
+    city = models.CharField(max_length=255)
+    state = us_models.USStateField()
+    zip = models.PositiveIntegerField()
+    
+    class Meta:
+        verbose_name_plural = 'addresses'
+    
+    def __unicode__(self):
+        return "%s, %s, %s %i" % \
+            (self.street, self.city, self.state, self.zip)
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    notes = models.TextField(null=True, blank=True)
+    picture = models.ImageField(null=True, blank=True, max_length=1048576, upload_to="picture/profile/")
+    
+    def primary_phone(self):
+        for type in ('office', 'mobile', 'home'):
+            for phone in self.phones.all():
+                if phone.type == type:
+                    return phone
+        
+        return None
+    
+    class Admin:
+        ordering = ('user__last_name', 'user__first_name',)
+        list_display = ['user', 'notes']
+    
+    class Meta:
+        permissions = (
+            ('view_profile', 'Can view profile'),
+            ('access_xmlrpc', 'Can access XML-RPC service')
+        )
+    
+    def __unicode__(self):
+        return self.user.get_full_name()
+
+
+class Phone(models.Model):
+    PHONE_TYPES = (
+        ('home', 'Home'),
+        ('office', 'Office'),
+        ('mobile','Mobile'),
+        ('fax', 'Fax')
+    )
+    
+    profile = models.ForeignKey(Profile, related_name='phones')
+    type = models.CharField(max_length=15, choices=PHONE_TYPES)
+    number = us_models.PhoneNumberField()
+
+    class Meta:
+        unique_together = (('profile', 'type'),)
+    
+    def __unicode__(self):
+        return self.number
+
+
+class BusinessType(models.Model):
+    name = models.CharField(max_length=255)
+    can_view_all_projects = models.BooleanField(
+        default=False,
+        help_text='Allow billable expenses to projects not associated with a business of this type.  For example, a billable expense for staying at a hotel, but will be billed to a client project.  When creating an exchange, this value specifies whether or not all projects show up under the Project drop down menu.'
+    )
+    
+    def __unicode__(self):
+        return self.name
+
+
+class BusinessManager(models.Manager):
+    def __init__(self, business_type):
+        super(BusinessManager, self).__init__()
+        self.business_type = business_type
+        
+    def get_query_set(self):
+        return super(BusinessManager, self).get_query_set().filter(
+            business_types__name__iexact=self.business_type
+        )
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.ForeignKey(Address, null=True, blank=True)
+    contacts = models.ManyToManyField(
+        User,
+        blank=True,
+        related_name='businesses',
+    )
+    notes = models.TextField(null=True, blank=True)
+    logo = models.ImageField(
+        null=True,
+        blank=True,
+        max_length=1048576,
+        upload_to='picture/logo/',
+    )
+    business_types = models.ManyToManyField(
+        BusinessType,
+        blank=True,
+        related_name='businesses',
+    )
+    
+    objects = models.Manager()
+    clients = BusinessManager('client')
+    vendors = BusinessManager('vendor')
+    creditors = BusinessManager('creditor')
+    members = BusinessManager('member')
+    
+    def _get_exchange_types(self):
+        # import here to avoid circular import
+        try:
+            from ledger.models import ExchangeType
+            return ExchangeType.objects.filter(
+                business_types__businesses=self
+            )
+        except ImportError:
+            return []
+    exchange_types = property(_get_exchange_types)
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'businesses'
+        ordering = ('name',)
+        permissions = (
+            ('view_business', 'Can view business'),
+        )
+
+
+class Project(models.Model):
+    PROJECT_STATUSES = (
+        ('requested', 'Requested'),
+        ('accepted', 'Accepted'),
+        ('finished', 'Finished'),
+    )
+    
+    PROJECT_TYPES = (
+        ('consultation', 'Consultation'),
+        ('software', 'Software Project'),
+    )
+    
+    name = models.CharField(max_length = 255)
+    trac_environment = models.CharField(max_length = 255, blank=True, null=True)
+    business = models.ForeignKey(Business, related_name='projects')
+    point_person = models.ForeignKey(User, limit_choices_to= {'is_staff':True})
+    contacts = models.ManyToManyField(
+        User,
+        related_name='projects',
+        through='ProjectRelationship',
+    )
+    
+    type = models.CharField(max_length=15, choices=PROJECT_TYPES)
+    status = models.CharField(max_length=15, choices=PROJECT_STATUSES)
+    description = models.TextField()
+    
+    class Meta:
+        ordering = ('name', 'status', 'type',)
+        permissions = (
+            ('view_project', 'Can view project'),
+            ('email_project_report', 'Can email project report'),
+        )
+    
+    def __unicode__(self):
+        return self.name
+    
+    def trac_url(self):
+        return settings.TRAC_URL % self.trac_environment
+
+
+class ProjectRelationship(models.Model):
+    types = models.ManyToManyField(
+        'ProjectRelationshipType',
+        related_name='project_relationships',
+        blank=True,
+    )
+    user = models.ForeignKey(User)
+    project = models.ForeignKey(Project)
+    
+    class Meta:
+        unique_together = ('user', 'project',)
+    
+    def __unicode__(self):
+        return "%s's relationship to %s" % (
+            self.project.name,
+            self.user.get_full_name(),
+        )
+
+
+class ProjectRelationshipType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.CharField(max_length=255, unique=True, editable=False)
+    
+    def save(self):
+        queryset = ProjectRelationshipType.objects.all()
+        if self.id:
+            queryset = queryset.exclude(id__exact=self.id)
+        self.slug = slugify_uniquely(self.name, queryset, 'slug')
+        super(ProjectRelationshipType, self).save()
+    
+    def __unicode__(self):
+        return self.name
+
+
+class Interaction(models.Model):
+    """ Communication log """
+    
+    INTERACTION_TYPES = (
+        ('email', 'Email'),
+        ('meeting', 'Meeting'),
+        ('phone', 'Phone'),
+        ('business', 'Business'),
+        ('exchange', 'Exchange'),
+    )
+
+    date = models.DateTimeField()
+    type = models.CharField(max_length=15, choices=INTERACTION_TYPES)
+    completed = models.BooleanField(default=False)
+    project = models.ForeignKey(Project, null=True, blank=True)
+    memo = models.TextField(null=True)
+    cdr_id = models.TextField(null=True)
+    
+    contacts = models.ManyToManyField(User, related_name='interactions')
+    
+    def src(self):
+        if self.cdr:
+            return self.cdr.src
+    src.short_description = 'Source'
+
+    def dst(self):
+        if self.cdr:
+            return self.cdr.dst
+    dst.short_description = 'Destination'
+
+    def duration(self):
+        if self.cdr:
+            time = self.cdr.duration / 60.0
+            return "%.2f minutes" % time
+    duration.short_description = 'Duration'
+    
+    class Meta:
+        ordering = ['-date']
+        permissions = (
+            ('view_interaction', 'Can view interaction'),
+            ('view_todo_list', 'Can view to do list'),
+        )
+
+    def __unicode__(self):
+        return "%s: %s" % ( self.date.strftime("%m/%d/%y"), self.type )
+
+def install():
+    group, created = Group.objects.get_or_create(name='CRM Admin')
+    if created:
+        perms = Permission.objects.filter(
+            content_type__in=ContentType.objects.filter(
+                models.Q(app_label='crm') | models.Q(app_label='auth')
+            ),
+        )
+        for perm in perms:
+            group.permissions.add(perm)
+    
+    group, created = Group.objects.get_or_create(name='Pagelet Admin')
+    if created:
+        for perm in Permission.objects.filter(codename__icontains='pagelet'):
+            group.permissions.add(perm)
