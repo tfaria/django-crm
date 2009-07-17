bbf478d84255d6a1b91183d0648da2eaa613041d
tobias.mcnulty <tobias.mcnulty@a8de2626-5439-11de-936b-e9512c4d2277>
2009-07-17 18:02:11 +0000

    adding basic admin for CRM app

---
+++ crm/admin.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# $Id: admin.py 433 2009-07-14 04:10:28Z tobias $
+# ----------------------------------------------------------------------------
+#
+#    Copyright (C) 2008 Caktus Consulting Group, LLC
+#
+#    This file is part of minibooks.
+#
+#    minibooks is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of 
+#    the License, or (at your option) any later version.
+#    
+#    minibooks is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#    
+#    You should have received a copy of the GNU Affero General Public License
+#    along with minibooks.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+
+from crm import models as crm
+
+class BusinessTypeAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(crm.BusinessType, BusinessTypeAdmin)
+
+
+class ProjectRelationshipType(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+admin.site.register(crm.ProjectRelationshipType, ProjectRelationshipType)
