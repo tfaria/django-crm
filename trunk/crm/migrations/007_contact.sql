0c84c4a9570ddd2ae027999f69c5839ba21e7f75
copelco <copelco@a8de2626-5439-11de-936b-e9512c4d2277>
2009-09-21 17:19:35 +0000

    add transactions to migrations

--- trunk/crm/migrations/007_contact.sql
+++ trunk/crm/migrations/007_contact.sql
@@ -1,5 +1,10 @@
 -- add NOT NULL constraint to contact notes and description
+BEGIN;
 UPDATE crm_contact SET notes = '' WHERE notes IS NULL;
-ALTER TABLE crm_contact ALTER COLUMN notes SET NOT NULL;
 UPDATE crm_contact SET description = '' WHERE description IS NULL;
+COMMIT;
+
+BEGIN;
+ALTER TABLE crm_contact ALTER COLUMN notes SET NOT NULL;
 ALTER TABLE crm_contact ALTER COLUMN description SET NOT NULL;
+COMMIT;
