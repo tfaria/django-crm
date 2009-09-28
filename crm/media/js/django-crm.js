033f4df66bfe802ed7078f7662fa872c5f842eda
tobias.mcnulty <tobias.mcnulty@a8de2626-5439-11de-936b-e9512c4d2277>
2009-09-28 18:41:01 +0000

    add comment to django-crm.js

--- crm/media/js/django-crm.js
+++ crm/media/js/django-crm.js
@@ -1,4 +1,7 @@
 jQuery(function($) {
+	/* use jQuery-UI to apply a date picker to all crm-date-fields.  If you
+	 * wish, substitute your JavaScript library of choice.
+	 */
 	$('.crm-date-field').datepicker({
 		'dateFormat': 'mm/dd/yy'
 	});
